1545a4d54c1f262a2e77a4daabe907f37c8be4eb
Near <neaix@hotmail.com>
2018-09-04 16:31:47 +0800

    init project.

---
+++ src/main/java/n/platform/Appliaction.java
@@ -0,0 +1,13 @@
+package n.platform;
+
+/**
+ * @Author: Near
+ * @Date: 2018/9/4 16:28
+ * @Description:≈¿≥Ê∆Ù∂Ø¿‡
+ */
+
+public class Appliaction {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
